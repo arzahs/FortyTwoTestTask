07a45ee9f2391dbfd5983aee3e8f9c7ef6a54576
Sergey Nelepa <nelepa1995@mail.ru>
2016-06-22 15:31:35 +0300

    ticket:3 fixed adding new request to page

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -5,23 +5,24 @@ setInterval(function(){
     id = $(list_item).data('id');
     $.ajax({
         type: 'GET',
-        url: '/request_list/',
-        data: {id: id}
-    }).success(function(responce){
-        if (!responce.requests || !responce.requests.length) {
-            return;
+        url: '/requests_list',
+        data: {id: id},
+        dataType: 'json'
+    }).success(function(response){
+        if (!response || !response.length) {
+            return false;
         }
-            handlerMessages(responce.requests);
+            handlerMessages(response);
     });
 }, 5000);
 
 var handlerMessages = function(data){
-    var requests = JSON.parse(data);
-    newRequest += requests.length();
+    var requests = jQuery.parseJSON(JSON.stringify(data));
+    newRequest += requests.length;
     document.title = newRequest + " new requests";
     $('h2').text(newRequest + " new requests");
     $.each(requests, function(i, item){
-        var listItemString = "<li data-id='"+requests[i].id+"'>"+requests[i].method+" "+requests[i].path+" "+requests[i].status_code+" "+requests[i].server_protocol+" "+content_len+"</li>"
+        var listItemString = "<li data-id='"+requests[i].id+"'>"+requests[i].method+" "+requests[i].path+" "+requests[i].status_code+" "+requests[i].server_protocol+" "+requests[i].content_len+"</li>"
         $('ul').prepend(listItemString);
     });
 

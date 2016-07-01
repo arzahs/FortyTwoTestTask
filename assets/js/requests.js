af1dd759c1a39806b899d7058fa4fb241f67ee19
Sergey Nelepa <nelepa1995@mail.ru>
2016-07-01 20:41:58 +0300

    ticket:13 added script that sort requests list

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -1,5 +1,27 @@
 var id = 0;
 var newRequest = 0;
+var type_sort = '';
+
+function sortTable($table, order){
+    if(order !== ''){
+        var $rows = $('tbody tr', $table);
+        $rows.sort(function (a, b) {
+        var keyA = $('.priority', a).val();
+        var keyB = $('.priority', b).val();
+        if (order=='asc') {
+            return (keyA > keyB) ? 1 : 0;
+        } else {
+            return (keyA > keyB) ? 0 : 1;
+        }
+    });
+    $.each($rows, function (index, row) {
+        $table.append(row);
+    });
+    }
+
+
+};
+
 setInterval(function(){
     var list_item =$('tbody tr')[0];
     id = $(list_item).data('id');
@@ -13,6 +35,8 @@ setInterval(function(){
             return false;
         }
         handlerMessages(response);
+
+
 
     });
 }, 5000);
@@ -29,6 +53,9 @@ var handlerMessages = function(data){
             $('tbody tr:last-child').remove();
         }
     });
+    if(type_sort !== ''){
+        sortTable($('table'), type_sort);
+    }
 
 };
 
@@ -40,7 +67,12 @@ $(document).ready(function () {
         $text.hide();
         $(this).children('input').val(priority).show();
         });
-
+    
+    $('#sort').on('change', function () {
+        type_sort = $(this).val();
+        sortTable($('table'), type_sort);
+    });
+    
     $('body').on('keyup', '.priority', function(event) {
         $(this).val($(this).val().replace(/[^+0-9]/gim,''));
         if(event.which == 13){
@@ -62,7 +94,8 @@ $(document).ready(function () {
                     if (!response || !response.length) {
                         return false;
                     }
-                    //sortItems
+                    
+                    sortTable($('table'), type_sort);
 
                 });
             }
